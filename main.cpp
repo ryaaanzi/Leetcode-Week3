caab9436bd9a370295d39c7b5325f8ce772f9996
Ryan Acebedo <ryaaanzi777@gmail.com>
2026-04-01 14:28:31 +0800

    Add Merge Two Sorted Lists solution with main and README

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
-#include <vector>
-#include "palindrome.cpp"
+#include "merge.h"
+
 using namespace std;
 
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val;
+        if (head->next != nullptr) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
 int main() {
+    ListNode* list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+    ListNode* list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
     Solution sol;
+    ListNode* merged = sol.mergeTwoLists(list1, list2);
 
-    int x;
-    cout << "Enter a number: ";
-    cin >> x;
-
-    if (sol.isPalindrome(x)) {
-        cout << "true" << endl;
-    } else {
-        cout << "false" << endl;
-    }
+    cout << "Merged List: ";
+    printList(merged);
 
     return 0;
 }
---
+++ merge.h
@@ -0,0 +1,11 @@
+#ifndef MERGE_H
+#define MERGE_H
+
+#include "ListNode.h"
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
+};
+
+#endif
